89dd93cd5fd759a89d92f3ecf17506a950429c35
t.novruzov <t.novruzov@STHQ012E-01>
2022-06-20 20:54:07 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Models;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public List<Human> People { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            People = new Faker<Human>()
+                .RuleFor(h=>h.Firstname, f=>f.Person.FirstName)
+                .RuleFor(h=>h.Lastname, f=>f.Person.LastName)
+                .RuleFor(h=>h.Gender, f=>f.Person.Gender.ToString())
+                .RuleFor(h=>h.Phone, f=>f.Person.Phone)
+                .RuleFor(h=>h.ImageUrl, f=>f.Person.Avatar)
+                .RuleFor(h=>h.Email, f=>f.Person.Email)
+                .RuleFor(h=>h.Address, f=>f.Person.Address.City)
+                .RuleFor(h=>h.DateOfBirth, f=>f.Person.DateOfBirth)
+                .Generate(10);
+
+            // listView.ItemsSource = People;
+        }
+    }
+}
---
+++ WpfApp1/Models/Human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WpfApp1.Models
+{
+    public class Human
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? Gender { get; set; }
+        public string? Phone { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+
+        //public override string ToString()
+        // => @$"{Firstname} - {Lastname} - {Gender} - {Phone} - {Email} - {Address}";
+
+    }
+}
